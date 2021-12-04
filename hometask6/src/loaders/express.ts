0f15235bf8accd4d35d7f3d57d367728ba27ed9e
Tran Bao Chau <tranbaochau2229@gmail.com>
2021-12-04 16:55:16 +0700

    done: hometask6 - implement cors

--- hometask6/src/loaders/express.ts
+++ hometask6/src/loaders/express.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 
 import config from "../config";
 import routes from "../api";
@@ -12,6 +13,7 @@ import { authenticationMiddleware } from "../api/middlewares/auth.middlewares";
 import { logger } from "../shared/logger";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use(httpLoggingMiddleware);
